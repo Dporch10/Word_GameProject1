a49d702fe557e81173a99643eb009b331b4f403b
DPorch10 <aintshit@DPorch10s-MacBook-Pro.local>
2024-01-06 10:02:46 -0500

    questions

---
+++ questions.js
@@ -0,0 +1,64 @@
+let questions = [
+    {
+    numb: 1,
+    question: "Which QB has the most Superbowl Wins?",
+    answer: "Tom Brady",
+    options: [
+        "Patrick Mahomes",
+        "Peyton Manning",
+        "Tom Brady",
+        "Drew Brees"
+    ]
+},
+    {
+     numb: 2,
+     question: "Who is the all-time leading scorer in the NBA?",
+     answer: "LeBron James",
+     options: [
+        "LeBron James",
+        "Kobe Bryant",
+        "Michael Jordan",
+        "Stephen Curry"
+     ]   
+    },
+
+    {
+        numb: 3, 
+        question: "What College does Deion Sanders coach for?",
+        answer: "Colorado Buffaloes",
+        options: [
+            "Florida State Seminoles",
+            "Carolina Tar Heels",
+            "Texas Longhorns",
+            "Colorado Buffaloes"
+        ]
+
+    },
+
+    {
+        numb: 4,
+        question: "How many holes are there in a game of golf?",
+        answer: "18",
+        options: [
+            "9",
+            "18",
+            "36",
+            "12"
+        ]
+    },
+
+    {
+        numb: 5, 
+        question: "Which sport does not use a ball",
+        answer: "Hockey",
+        options: [
+            "Tennis",
+            "Hockey",
+            "Ping Pong",
+            "Golf"
+        ]
+
+    }
+
+
+];
