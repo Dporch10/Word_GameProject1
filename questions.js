4f1b1430986e3186fdab01ec54d39abc173c7ef4
DPorch10 <aintshit@DPorch10s-MBP.lan1>
2024-01-06 23:10:46 -0500

    done

--- questions.js
+++ questions.js
@@ -1,64 +1,58 @@
 let questions = [
     {
     numb: 1,
-    question: "Which QB has the most Superbowl Wins?",
+    question: "Which Quarterback has the most Superbowl Wins?",
     answer: "Tom Brady",
     options: [
-        "Patrick Mahomes",
-        "Peyton Manning",
-        "Tom Brady",
-        "Drew Brees"
+      "Patrick Mahomes",
+      "Peyton Manning",
+      "Tom Brady",
+      "Drew Brees"
     ]
-},
+  },
     {
-     numb: 2,
-     question: "Who is the all-time leading scorer in the NBA?",
-     answer: "LeBron James",
-     options: [
-        "LeBron James",
-        "Kobe Bryant",
-        "Michael Jordan",
-        "Stephen Curry"
-     ]   
-    },
-
+    numb: 2,
+    question: "Who is the all-time leading scorer in the NBA?",
+    answer: "LeBron James",
+    options: [
+      "LeBron James",
+      "Kobe Bryant",
+      "Michael Jordan",
+      "Larry Bird"
+    ]
+  },
     {
-        numb: 3, 
-        question: "What College does Deion Sanders coach for?",
-        answer: "Colorado Buffaloes",
-        options: [
-            "Florida State Seminoles",
-            "Carolina Tar Heels",
-            "Texas Longhorns",
-            "Colorado Buffaloes"
-        ]
-
-    },
-
+    numb: 3,
+    question: "What College does Deion Sanders coach for?",
+    answer: "Colorado Buffaloes",
+    options: [
+      "Florida State Seminoles",
+      "Colorado Buffaloes",
+      "Texas Longhorns",
+      "Washington Huskies"
+    ]
+  },
     {
-        numb: 4,
-        question: "How many holes are there in a game of golf?",
-        answer: "18",
-        options: [
-            "9",
-            "18",
-            "36",
-            "12"
-        ]
-    },
-
+    numb: 4,
+    question: "How many holes are there in a round of golf?",
+    answer: "18",
+    options: [
+      "9",
+      "12",
+      "36",
+      "18"
+    ]
+  },
     {
-        numb: 5, 
-        question: "Which sport does not use a ball",
-        answer: "Hockey",
-        options: [
-            "Tennis",
-            "Hockey",
-            "Ping Pong",
-            "Golf"
-        ]
-
-    }
-
+    numb: 5,
+    question: "What sport does not use a ball?",
+    answer: "Hockey",
+    options: [
+      "Ping Pong",
+      "Hockey",
+      "Soccer",
+      "Bowling"
+    ]
+  },
 
 ];
